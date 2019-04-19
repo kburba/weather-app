56727cfe87f8fc3f22cb8dc7296d7dea383d2cc6
Kestutis Burba <kburba@gmail.com>
2019-04-19 05:27:50 +0300

    http to https

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,3 +1,3 @@
-export const OWMurl = "http://api.openweathermap.org/data/2.5/weather";
+export const OWMurl = "https://api.openweathermap.org/data/2.5/weather";
 export const OWMkey = "cb8528b6f2cf632a85f5e8f1e9dc4a0b";
 export const gMapKey = "AIzaSyDwPWp83c8aD086C5tLxD10kCErHt9lpTo&";
